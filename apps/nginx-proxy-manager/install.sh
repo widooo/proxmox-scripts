0bc3a74a9f17bda11b2de8b964034d7796527202
Wido Potters <wido.github@herenstraat.nl>
2026-07-12 18:23:02 +0200

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -213,7 +213,7 @@ step_start "Yarn"
 step_start "Nginx Proxy Manager" "Downloading" "Downloaded"
   NPM_VERSION="2.15.1"
   os_fetch -O- https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v$NPM_VERSION
-  tar -xvf v2.15.1
+  tar -xz v$NPM_VERSION
   cd ./nginx-proxy-manager-$NPM_VERSION
   step_end "Nginx Proxy Manager ${CLR_CYB}v$NPM_VERSION${CLR} ${CLR_GN}Downloaded"
 
